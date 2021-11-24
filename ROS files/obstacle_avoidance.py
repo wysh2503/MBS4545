bb60add34abfffe37d7ea231df7d51cd31223545
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-11-23 20:18:57 -0800

    Update obstacle_avoidance.py

--- ROS files/obstacle_avoidance.py
+++ ROS files/obstacle_avoidance.py
@@ -12,12 +12,12 @@ def callback(msg):
   rospy.loginfo(rospy.get_caller_id() + " The distance to obstacle is -  %s",msg.ranges[300])
 
 #If the distance to an obstacle in front of the robot is larger than 1 meter, the robot will keep moving forward
-  if msg.ranges[300] > disToObstacle:
+  if msg.ranges[180] > disToObstacle:
       move.linear.x = 0.5
       move.angular.z = 0.0
 
 #If the distance to an obstacle in front of the robot is less than 1 meter, the robot will turn left
-  if msg.ranges[300] <= disToObstacle: 
+  if msg.ranges[180] <= disToObstacle: 
       move.linear.x = 0.0
       move.angular.z = 0.5
 
