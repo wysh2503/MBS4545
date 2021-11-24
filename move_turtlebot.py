b86799b36cf362d1ce2a0ed09a2f59fbb61d1ab6
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-11-23 19:49:26 -0800

    Update move_turtlebot.py

--- move_turtlebot.py
+++ move_turtlebot.py
@@ -3,11 +3,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-rospy.init_node('topic_publisher')
-pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
+rospy.init_node('move_bot_node')
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 rate = rospy.Rate(2)
 move = Twist()
-move.linear.x = 0.5
+move.linear.x = 0
 move.angular.z = 0.5
 
 while not rospy.is_shutdown(): 
